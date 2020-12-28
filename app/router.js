b6d38f03cbf41c7c029dec8d236d19c25d7930fb
jiajun <jiajun.qin@brgroup.com>
2020-12-28 20:01:43 +0800

    init eggjs-demo

---
+++ app/router.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * @param {Egg.Application} app - egg application
+ */
+module.exports = app => {
+  const { router, controller } = app;
+  router.resources('topics', '/api/v2/topics', controller.topics);
+  router.get('/', controller.home.index);
+};
